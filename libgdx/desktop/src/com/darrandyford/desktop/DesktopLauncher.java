7f7aefd764a10bde738851f0f4e7ca7feec09d28
bedfordwest <bedfordwest@gmail.com>
2018-01-08 12:28:41 -0600

    feat(desktop_config): Added texture packing and resolution for desktop

--- libgdx/desktop/src/com/darrandyford/desktop/DesktopLauncher.java
+++ libgdx/desktop/src/com/darrandyford/desktop/DesktopLauncher.java
@@ -2,11 +2,33 @@ package com.darrandyford.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 import com.darrandyford.DarrAndyFord;
+import com.darrandyford.utils.Constants;
 
 public class DesktopLauncher {
+	private static boolean rebuildAtlas = true;
+	private static boolean drawDebugOutline = false;
+
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		if (rebuildAtlas) {
+			TexturePacker.Settings settings = new TexturePacker.Settings();
+			settings.maxWidth = 1024;
+			settings.maxHeight = 1024;
+			settings.duplicatePadding = true;
+			settings.debug = drawDebugOutline;
+			settings.combineSubdirectories = true;
+			TexturePacker.process(settings, "./",
+				"atlas", "worldtextures");
+		}
+
+		LwjglApplicationConfiguration config =
+			new LwjglApplicationConfiguration();
+
+		config.title = "DarrAndyFord Development Build v0.1";
+		config.width = Constants.DESKTOP_WIDTH;
+		config.height = Constants.DESKTOP_HEIGHT;
+
 		new LwjglApplication(new DarrAndyFord(), config);
 	}
 }
