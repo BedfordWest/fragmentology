e7c198f90a5e50754a6f8a2035002e0980960b0a
bedfordwest <bedfordwest@gmail.com>
2018-01-15 20:44:15 -0600

    fix(imports): Remove unused imports

--- libgdx/core/src/com/darrandyford/world/WorldListener.java
+++ libgdx/core/src/com/darrandyford/world/WorldListener.java
@@ -1,8 +1,5 @@
 package com.darrandyford.world;
 
-import box2dLight.ConeLight;
-import box2dLight.Light;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 import com.darrandyford.entities.living.characters.Enemy;
 import com.darrandyford.entities.living.characters.Player;
