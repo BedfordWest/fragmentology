bf909951842f2d8024f6d26ca9442b62cf55c924
bedfordwest <bedfordwest@gmail.com>
2018-01-21 00:13:36 -0600

    chore(background): Darkened the background render color

--- libgdx/core/src/com/darrandyford/DarrAndyFord.java
+++ libgdx/core/src/com/darrandyford/DarrAndyFord.java
@@ -51,8 +51,8 @@ public class DarrAndyFord implements ApplicationListener {
 		}
 
 		// Sets the clear screen color to: Cornflower Blue
-		Gdx.gl.glClearColor(0x64/255.0f, 0x95/255.0f,
-			0xed/255.0f, 0xff/255.0f);
+		Gdx.gl.glClearColor(0x30/255.0f, 0x20/255.0f,
+			0x25/255.0f, 0xff/255.0f);
 		// Clears the screen
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
