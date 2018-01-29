ca5bcc585bea04131f8a418d0451e64bb160a48c
Bedford West <bedfordwest@gmail.com>
2018-01-29 09:12:07 -0600

    Feat/junit (#119)
    * feat(junit): Introduce junit to the project
    
    * feat(unit_tests): Changed gradle files to allow for unit tests and added a WorldRendereTest class

---
+++ libgdx/core/src/test/java/com/darrandyford/world/WorldRendererTest.java
@@ -0,0 +1,36 @@
+package com.darrandyford.world;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WorldRendererTest {
+
+	@Test
+	void render() {
+	}
+
+	@Test
+	void renderWorld() {
+	}
+
+	@Test
+	void renderGUI() {
+	}
+
+	@Test
+	void resize() {
+	}
+
+	@Test
+	void dispose() {
+	}
+
+	@Test
+	void toggleDebugDrawBox2dWorld() {
+	}
+
+	@Test
+	void getDebugDrawBox2dWorld() {
+	}
+}
